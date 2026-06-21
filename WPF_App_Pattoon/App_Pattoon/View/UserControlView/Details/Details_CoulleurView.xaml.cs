cb808c8fe4b9d53f99f06f03be2733268115bed4
Apollo <gohdayappolinaire.nguessan@student.efpl.be>
2026-06-21 11:56:13 +0200

    Add files via upload

---
+++ WPF_App_Pattoon/App_Pattoon/View/UserControlView/Details/Details_CoulleurView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_App_Pattoon_Animalerie.View.UserControlView.Details
+{
+    /// <summary>
+    /// Logique d'interaction pour Details_CoulleurView.xaml
+    /// </summary>
+    public partial class Details_CoulleurView : UserControl
+    {
+        public Details_CoulleurView()
+        {
+            InitializeComponent();
+        }
+    }
+}
